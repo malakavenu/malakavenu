69fc58bc6542a254522a1ab67d38c85501faa6c2
Reddy <492613@CTS.COM>
2016-10-12 15:09:30 +0530

    v

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         <meta name="author" content="Malaka Venugopal Reddy Front End Developer">
         <meta name="keywords" content="Malaka Venugopal Reddy / Software Engineer">
      	<title> Malaka Venugopal Reddy / Software Engineer </title>
-      	<link rel="stylesheet" href="css/font-awesome.min">
+      	<link rel="stylesheet" href="css/font-awesome.min.css">
       	<link rel="stylesheet" href="css/app.css?v-1.0">
       	<!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
